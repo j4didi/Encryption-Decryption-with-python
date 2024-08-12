fc847715bdd42fc197ef21809575aaa13a17f0ba
Moho <mohojadidi@gmail.com>
2024-08-12 10:49:25 +0330

    add 1 - 1,000,000 to database

--- add_hash_to_db.py
+++ add_hash_to_db.py
@@ -1,13 +1,9 @@
 from functions import database_md5, hash_encryption
-import itertools
 
-letters = '1234567890'
-
-all_combinations = []
-for r in range(3, 7):
-    combinations = itertools.product(letters, repeat=r)
-    all_combinations.extend([''.join(combination) for combination in combinations])
-
-for word in all_combinations:
-        database_md5(word,hash_encryption(word))
+for word in range(1, 1000000):
+    word = str(word)
+    try:
+        database_md5(word, hash_encryption(word))
         print(word)
+    except Exception as e:
+        print(e)
